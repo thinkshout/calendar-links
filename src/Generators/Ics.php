de1075243a6ebfe82739feb3eeb7e3db4b9242d3
Jaymz Rhime <wxactly@gmail.com>
2023-03-01 07:38:33 -0600

    Using UTC dates for ICS file, patch for 1.8.2 tag only

--- src/Generators/Ics.php
+++ src/Generators/Ics.php
@@ -12,7 +12,7 @@ class Ics implements Generator
 {
     /** @var string {@see https://www.php.net/manual/en/function.date.php} */
     protected $dateFormat = 'Ymd';
-    protected $dateTimeFormat = 'e:Ymd\THis';
+    protected $dateTimeFormat = 'Ymd\THis';
 
     /** @var array<non-empty-string, non-empty-string> */
     protected $options = [];
@@ -40,13 +40,13 @@ class Ics implements Generator
         $dateTimeFormat = $link->allDay ? $this->dateFormat : $this->dateTimeFormat;
 
         if ($link->allDay) {
-            $url[] = 'DTSTAMP;TZID='.$link->from->format($dateTimeFormat);
-            $url[] = 'DTSTART:'.$link->from->format($dateTimeFormat);
+            $url[] = 'DTSTAMP:'.gmdate($dateTimeFormat, $link->from->getTimestamp()).'Z';
+            $url[] = 'DTSTART:'.gmdate($dateTimeFormat, $link->from->getTimestamp()).'Z';
             $url[] = 'DURATION:P'.(max(1, $link->from->diff($link->to)->days)).'D';
         } else {
-            $url[] = 'DTSTAMP;TZID='.$link->from->format($dateTimeFormat);
-            $url[] = 'DTSTART;TZID='.$link->from->format($dateTimeFormat);
-            $url[] = 'DTEND;TZID='.$link->to->format($dateTimeFormat);
+            $url[] = 'DTSTAMP:'.gmdate($dateTimeFormat, $link->from->getTimestamp()).'Z';
+            $url[] = 'DTSTART:'.gmdate($dateTimeFormat, $link->from->getTimestamp()).'Z';
+            $url[] = 'DTEND:'.gmdate($dateTimeFormat, $link->to->getTimestamp()).'Z';
         }
 
         if ($link->description) {
